98d22c409fc65dc4ff431bdf4aae8f661c861971
ShifaMS <shaikhshifa01@gmail.com>
2022-06-30 10:32:24 +0530

    AccPageGitPush class update

---
+++ src/main/java/com/qa/opencart/pages/AccpageGitPush.java
@@ -0,0 +1,10 @@
+package com.qa.opencart.pages;
+
+public class AccpageGitPush {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Git Push Acc page");
+	}
+
+}
